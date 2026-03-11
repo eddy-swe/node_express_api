639e852d115dde2ce602dbbe4c14a7be8c513d15
Eddy Ngeno <eddyngeno12@gmail.com>
2026-03-11 08:07:53 +0300

    Refactor users route to utilize controller functions and remove inline logic

--- routes/users.js
+++ routes/users.js
@@ -1,44 +1,17 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import { createUser, getUsers, getUser, deleteUser, updateUser } from '../conrollers/users.js';
 
 const router = express.Router();
 
-let users = [];
+router.get('/', getUsers);
 
-router.get('/', (req, res) => {
-    res.send(users);
-});
+router.post('/', createUser);
 
-router.post('/', (req, res) => {
-    const user = req.body;
+router.get('/:id', getUser)
 
-    users.push({ ...user, id: uuidv4() });
+router.delete('/:id', deleteUser);
 
-    res.send(`User with the username ${user.firstName} added to the database!`);
-});
-
-// /users/2 ---> req.params { id: 2 }
-
-router.get('/:id', (req, res) => {
-    const {id} = req.params;
-
-    const foundUser = users.find ((user) => user.id === id );
-
-    res.send(foundUser);
-})
-
-router.delete('/:id', (req,res) => {
-    const {id} = req.params;
-
-    users = users.filter((user) => user.id != id);
-
-    res.send(`User with the id ${id} deleted from the database`);
-});
-
-router.patch('/:id', (req, res) => {
-    const {id} = req.params;
-
-    const user = users.find((user) => user.id === id);
-});
+router.patch('/:id', updateUser);
 
 export default router;
